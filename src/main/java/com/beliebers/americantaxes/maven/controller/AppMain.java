42a453d699a6904f7592b9af75e4324b372e06d8
xartez <jakubchludzinski@gmail.com>
2017-03-15 15:32:01 +0100

    poprawki

--- src/main/java/com/beliebers/americantaxes/maven/controller/AppMain.java
+++ src/main/java/com/beliebers/americantaxes/maven/controller/AppMain.java
@@ -1,6 +1,11 @@
 package com.beliebers.americantaxes.maven.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.beliebers.americantaxes.maven.product.model.Product;
+import com.beliebers.americantaxes.state.State;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +14,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class AppMain extends Application {
-
+	private static List<Product> productList = new ArrayList<Product>();
+	private static ArrayList<State> statesList = new ArrayList<State>();
 	private Stage primaryStage;
 	private AnchorPane rootLayout;
 	
@@ -23,6 +29,17 @@ public class AppMain extends Application {
 	
 	public void initRootLayout() {
         try {
+        	productList.add(new Product("Gruszki", 4.5));
+    		productList.add(new Product("Grzyby", 40));
+    		productList.add(new Product("Pizza z kurczakiem", 12.40));
+    		productList.add(new Product("Kiełbasa", 14.90));
+    		productList.add(new Product("Chleb", 2.70));
+
+    		statesList.add(new State("Alabama", 0.04, 0.135, 0.135));
+    		statesList.add(new State("Alaska", 0, 0.07, 0.07));
+    		statesList.add(new State("Arizona", 0.056, 0.10725, 0.10725));
+    		statesList.add(new State("Arkansas", 0.065, 0.11625, 0.11625));
+    		statesList.add(new State("California", 0.0725, 0.0975, 0.0975));
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(AppMain.class.getResource("MainWindow.fxml"));
@@ -38,6 +55,7 @@ public class AppMain extends Application {
     }
 
 	public static void main(String[] args) {
+		
 		launch(args);
 	}
 }
