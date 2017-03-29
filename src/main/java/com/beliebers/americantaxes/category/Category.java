778619e7ec1c332e033e90f5d0045b6f3d5cac01
Aleksander Kaczmarczyk <jakubchludzinski@gmail.com>
2017-03-29 14:16:29 +0200

    Category - zmiana typu mapy

--- src/main/java/com/beliebers/americantaxes/category/Category.java
+++ src/main/java/com/beliebers/americantaxes/category/Category.java
@@ -5,13 +5,13 @@ import java.util.Map;
 public class Category {
 	
 	private String name;
-	private Map<String,Double> typeAndTax;
+	private Map<Integer,Double> typeAndTax;
 	
 	public Category(String name) {
 		this.name = name;
 	}
 	
-	public Category(String name, Map<String, Double> typeAndTax) {
+	public Category(String name, Map<Integer, Double> typeAndTax) {
 		super();
 		this.name = name;
 		this.typeAndTax = typeAndTax;
@@ -25,11 +25,11 @@ public class Category {
 		this.name = name;
 	}
 
-	public Map<String, Double> getTypeAndTax() {
+	public Map<Integer, Double> getTypeAndTax() {
 		return typeAndTax;
 	}
 
-	public void setTypeAndTax(Map<String, Double> typeAndTax) {
+	public void setTypeAndTax(Map<Integer, Double> typeAndTax) {
 		this.typeAndTax = typeAndTax;
 	}
 	
