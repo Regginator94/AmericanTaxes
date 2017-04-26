2aaf85a9470f06e75f5ad76805bc555bfbd482d2
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-04-26 15:16:18 +0200

    Bugfix podatkow

--- src/main/java/com/beliebers/americantaxes/maven/controller/MainWindowController.java
+++ src/main/java/com/beliebers/americantaxes/maven/controller/MainWindowController.java
@@ -13,6 +13,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
