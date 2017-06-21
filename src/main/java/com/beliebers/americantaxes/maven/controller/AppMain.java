a991b26c0e84fb1c96d4044af81590c8dd99638d
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-06-21 15:48:44 +0200

    bug fixes

--- src/main/java/com/beliebers/americantaxes/maven/controller/AppMain.java
+++ src/main/java/com/beliebers/americantaxes/maven/controller/AppMain.java
@@ -1,19 +1,10 @@
 package com.beliebers.americantaxes.maven.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.beliebers.americantaxes.category.Category;
-import com.beliebers.americantaxes.maven.product.model.Product;
-import com.beliebers.americantaxes.state.State;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -38,7 +29,7 @@ public class AppMain extends Application {
      	
     		mainController.setMainApp(this);
         	FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(AppMain.class.getResource("MainWindow.fxml"));
+            loader.setLocation(this.getClass().getResource("MainWindow.fxml"));
             rootLayout = (AnchorPane) loader.load();
           
            
--- src/main/java/com/beliebers/americantaxes/maven/controller/MainWindowController.java
+++ src/main/java/com/beliebers/americantaxes/maven/controller/MainWindowController.java
@@ -16,8 +16,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class MainWindowController {
